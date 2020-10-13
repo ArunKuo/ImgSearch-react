191da63452ebffcb4f5193793d682ef2daeaa9a5
ArunKuo <steins1119@gmail.com>
2020-10-13 15:30:10 +0800

    Updates

---
+++ precache-manifest.420c7865939efd3d562f5820f64b774b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb4de0c19733a7aad4771291bd17631",
+    "url": "/ImgSearch-react/index.html"
+  },
+  {
+    "revision": "2da3ea3663d30ad70c8d",
+    "url": "/ImgSearch-react/static/css/main.8dcb1a70.chunk.css"
+  },
+  {
+    "revision": "83780c69b5c5574b9273",
+    "url": "/ImgSearch-react/static/js/2.e752a839.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ImgSearch-react/static/js/2.e752a839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2da3ea3663d30ad70c8d",
+    "url": "/ImgSearch-react/static/js/main.6e0fbd37.chunk.js"
+  },
+  {
+    "revision": "56417b9fca52e28e8a2f",
+    "url": "/ImgSearch-react/static/js/runtime-main.f8a53857.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ImgSearch-react/precache-manifest.420c7865939efd3d562f5820f64b774b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ImgSearch-react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
